cfbf3651dfdab5906d5799186d5e3c35fc5e79e4
marcelrojo <rojewskimarcel@gmail.com>
2024-05-11 18:47:35 +0200

    Add floating point conversion and unit tests

--- src/main/java/pl/put/poznan/transformer/logic/textDecorators/Numbers2TextDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/textDecorators/Numbers2TextDecorator.java
@@ -10,24 +10,57 @@ public class Numbers2TextDecorator extends TextDecorator {
     public Numbers2TextDecorator(TextTransformer textToTransform) {
         super(textToTransform);
     }
-    
-//    public static void main(String[] args){
-//        String test_text="I have 1 apple, 15 watermelons, 27 bananas, 100 pears, 257 oranges, 985 grapes and 1000 issues";
-//        TextTransformer decoratedTransformer = null;
-//        decoratedTransformer= new Numbers2TextDecorator(decoratedTransformer);
-//        System.out.println(test_text);
-//        System.out.println(decoratedTransformer.transform(test_text));
-//    }
+
+
+    private String number2text(String number) {
+        String[] single_digits = new String[]{"zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"};
+        String[] teens = new String[]{"ten", "eleven", "twelve", "thirteen", "fourteen",
+                "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+        String[] double_digits = new String[]{"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+        String temp = "";
+        int n = number.length();
+        if (n == 1) {
+            temp = single_digits[number.charAt(0) - '0'];
+        } else if (n == 2) {
+            if (number.charAt(0) == '1') {
+                temp = teens[number.charAt(1) - '0'];
+            } else {
+                temp = double_digits[number.charAt(0) - '2'];
+                if (number.charAt(1) != '0') {
+                    temp += "-" + single_digits[number.charAt(1) - '0'];
+                }
+            }
+        } else if (n == 3) {
+            temp = single_digits[number.charAt(0) - '0'] + " hundred";
+            if (number.charAt(1) != '0') {
+                temp += " ";
+                if (number.charAt(1) == '1') {
+                    temp += teens[number.charAt(2) - '0'];
+                } else {
+                    temp += double_digits[number.charAt(1) - '2'];
+                    if (number.charAt(2) != '0') {
+                        temp += "-" + single_digits[number.charAt(2) - '0'];
+                    }
+                }
+            } else if (number.charAt(2) != '0') {
+                temp += " " + single_digits[number.charAt(2) - '0'];
+            }
+
+        } else if (number.equals("1000")) {
+            temp = "one thousand";
+        } else {
+            temp = number;
+        }
+        return temp;
+    }
 
     @Override
-    public String transform(String text){
-        String [] single_digits = new String[] { "zero", "one", "two", "three", "four",
-                "five", "six", "seven", "eight", "nine"};
-        String [] teens= new String[] { "ten", "eleven", "twelve", "thirteen", "fourteen",
-                "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-        String[] double_digits= new String[] { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    public String transform(String text) {
 
-        Pattern pattern = Pattern.compile("\\b\\d+\\b");
+
+        Pattern pattern = Pattern.compile("\\b\\d+(\\.\\d+)?\\b");
         Matcher matcher = pattern.matcher(text);
         StringBuffer sb = new StringBuffer();
         String number;
@@ -36,42 +69,30 @@ public class Numbers2TextDecorator extends TextDecorator {
         while (matcher.find()) {
 
             number = matcher.group();
-            int n = number.length();
-            String temp = "";
-            if (n == 1) {
-                temp = single_digits[number.charAt(0) - '0'];
-            } else if (n == 2) {
-                if (number.charAt(0) == '1') {
-                    temp = teens[number.charAt(1) - '0'];
-                } else {
-                    temp = double_digits[number.charAt(0) - '2'];
-                    if (number.charAt(1) != '0') {
-                        temp += "-" + single_digits[number.charAt(1) - '0'];
-                    }
-                }
-            } else if( n == 3){
-                temp = single_digits[number.charAt(0) - '0'] + " hundred";
-                if (number.charAt(1) != '0') {
-                    temp += " ";
-                    if (number.charAt(1) == '1') {
-                        temp += teens[Integer.parseInt(number.substring(2)) - 10];
+            String temp = null;
+
+            if (number.contains(".")) {
+                String whole = number.substring(0, number.indexOf('.'));
+                String decimal = number.substring(number.indexOf('.') + 1);
+                int len_float = decimal.length();
+                if (len_float == 1 && !whole.equals(number2text(whole))) {
+                    if (decimal.equals("0")) {
+                        temp = number2text(whole);
                     } else {
-                        temp += double_digits[number.charAt(1) - '2'];
-                        if (number.charAt(2) != '0') {
-                            temp += "-" + single_digits[number.charAt(2) - '0'];
-                        }
+                        temp = number2text(whole) + " and " + number2text(decimal) + " tenths";
                     }
+                } else if (len_float == 2 && !whole.equals(number2text(whole))) {
+                    if (decimal.equals("00")) {
+                        temp = number2text(whole);
+                    } else {
+                        temp = number2text(whole) + " and " + number2text(decimal) + " hundredths";
+                    }
+                } else {
+                    temp = number;
                 }
-                else if (number.charAt(2) != '0') {
-                    temp += " " + single_digits[number.charAt(2) - '0'];
-                }
-                
-            }
-            else if (n == 4) {
-                temp="one thousand";
-            }
-            else{
-                temp="a lot";
+
+            } else {
+                temp = number2text(number);
             }
 
             matcher.appendReplacement(sb, temp);
