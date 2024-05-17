7608d39fe6196c6fd51f251ff5e74a9d68019c5a
marcelrojo <rojewskimarcel@gmail.com>
2024-05-17 20:49:05 +0200

    Add replacement of chosen expressions with acronyms

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformerSwitch.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformerSwitch.java
@@ -2,6 +2,7 @@ package pl.put.poznan.transformer.logic;
 
 import pl.put.poznan.transformer.logic.textDecorators.Numbers2TextDecorator;
 import pl.put.poznan.transformer.logic.textDecorators.ReverseDecorator;
+import pl.put.poznan.transformer.logic.textDecorators.Words2AcronymsDecorator;
 
 import java.util.Objects;
 
@@ -14,9 +15,11 @@ public class TextTransformerSwitch {
                 case "Reverse":
                     decoratedTransformer = new ReverseDecorator(decoratedTransformer);
                     break;
-
                 case "Numbers2Text":
                     decoratedTransformer = new Numbers2TextDecorator(decoratedTransformer);
+                    break;
+                case "Words2Acronyms":
+                    decoratedTransformer = new Words2AcronymsDecorator(decoratedTransformer);
                     break;
                 default: throw new IllegalArgumentException("Wrong transformer name");
             }
