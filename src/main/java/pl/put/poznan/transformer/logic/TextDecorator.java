7e7fea662688eb566c68881961ad92c7031e68cc
marcin <marcin.kapiszewski@gmail.com>
2024-05-27 17:37:28 +0200

    Add documentation for NumbersToTextDecorator TextDecorator and TextTransformerSwitch

--- src/main/java/pl/put/poznan/transformer/logic/TextDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextDecorator.java
@@ -1,11 +1,25 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * The TextDecorator class is an implementation of the TextTransformer interface.
+ * It decorates the text transformation functionality provided by another TextTransformer object.
+ */
 public class TextDecorator implements TextTransformer{
     protected final TextTransformer textToTransform;
 
+    /**
+     * Constructs a TextDecorator object with the specified TextTransformer object.
+     * @param textToTransform The TextTransformer object to decorate.
+     */
     public TextDecorator(TextTransformer textToTransform){
         this.textToTransform = textToTransform;
     }
+
+    /**
+     * Transforms the given text by delegating the transformation to the decorated TextTransformer object.
+     * @param text The text to transform.
+     * @return The transformed text.
+     */
     @Override
     public String transform(String text){
         return textToTransform.transform(text);
